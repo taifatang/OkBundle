a3954c9ea8b0d1261ae0e85bb9d6e0176462d68f
taifatang <taifatang@hotmail.com>
2017-01-14 15:38:06 +0000

    Init Commit

---
+++ OkBundle.UnitTests/OkTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Should;
+
+namespace OkBundle.UnitTests
+{
+    [TestFixture]
+    public class OkTests
+    {
+        [TestCase(true, true)]
+        [TestCase(false, false)]
+        [TestCase(!true, false)]
+        [TestCase(!false, true)]
+        public void If_Method_Return_Expected_Value(bool actual, bool expected)
+        {
+            bool result = Ok.If(actual);
+            result.ShouldEqual(expected);
+        }
+
+        [TestCase(1 == 1, true)]
+        [TestCase(1 == 2, false)]
+        [TestCase(true && true && false, false)]
+        [TestCase(true || true || false, true)]
+        public void If_Should_Take_Predicate(bool predicate, bool expected)
+        {
+            bool result = Ok.If(predicate);
+            result.ShouldEqual(expected);
+        }
+        [TestCase(true, 0)]
+        [TestCase(false, 1)]
+        public void If_Perform_Do_Block_According_To_Predicate(bool predicate, int expected)
+        {
+            var result = 1;
+
+            Ok.If(predicate)
+                .Do(() => { --result; });
+
+            result.ShouldEqual(expected);
+        }
+        [TestCase(true, 0)]
+        [TestCase(false, 2)]
+        public void If_Perform_Else_Block_According_To_Predicate(bool predicate, int expected)
+        {
+            var result = 1;
+
+            Ok.If(predicate)
+                .Do(() => { --result; })
+                .Else(() => { ++result; });
+
+            result.ShouldEqual(expected);
+        }
+    }
+}
